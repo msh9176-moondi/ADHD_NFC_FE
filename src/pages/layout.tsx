63391ddd8acf95bb03cc63fcb5dddff50acff444
imjane72-lab <imjane72@gmail.com>
2026-01-16 16:32:52 +0900

    광고

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -5,26 +5,31 @@ import { Outlet } from "react-router-dom";
 function RootLayout() {
   return (
     <div className="page">
-      <AppHeader />
+      {/* 전체 컨테이너 - 광고(256px) + 메인(412px) + 여백 */}
+      <div className="w-full max-w-175 mx-auto flex flex-col h-full px-4">
+        {/* 헤더 */}
+        <AppHeader />
 
-      {/* 가로 배치 컨테이너 */}
-      <div className="flex-1 w-full flex justify-center">
-        {/* 왼쪽 광고 */}
-        <aside className="w-64">
-          <div className="sticky top-4">
-            <div className="bg-white h-96 rounded-lg flex items-center justify-center">
-              광고 영역 (250x400)
+        {/* 메인 영역 */}
+        <div className="flex-1 w-full flex gap-6">
+          {/* 왼쪽 광고 */}
+          <aside className="w-64 shrink-0">
+            <div className="sticky top-4">
+              <div className="h-96 rounded-lg flex items-center justify-center border border-gray-200">
+                광고 영역
+              </div>
             </div>
-          </div>
-        </aside>
+          </aside>
 
-        {/* 메인 컨텐츠 */}
-        <main className="container flex-1">
-          <Outlet />
-        </main>
+          {/* 메인 컨텐츠 */}
+          <main className="container">
+            <Outlet />
+          </main>
+        </div>
+
+        {/* 푸터 */}
+        <AppFooter />
       </div>
-
-      <AppFooter />
     </div>
   );
 }
