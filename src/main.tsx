75119c6f797233db80cad8cc65094147c4d5d3ad
imjane72-lab <imjane72@gmail.com>
2026-01-16 13:22:25 +0900

    feat: dom 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,32 +1,32 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import './index.css';
-import App from './App.tsx';
-import LoginPage from './pages/auth/Login.tsx';
-import SignupPage from './pages/auth/Signup.tsx';
-import SplashPage from './pages/auth/Splash.tsx';
-import GrowthPage from './pages/growth/Growth.tsx';
-import MarketPage from './pages/market/Market.tsx';
-import CartpagePage from './pages/market/order/Cartpage.tsx';
-import CheckoutPage from './pages/market/order/Checkout.tsx';
-import CheckoutSuccessPage from './pages/market/order/CheckoutSuccess.tsx';
-import BranchingTestPage from './pages/market/test/BranchingTest.tsx';
-import ComplexTypePage from './pages/market/test/branching questions/ComplexType.tsx';
-import EmotionalTypePage from './pages/market/test/branching questions/EmotionalType.tsx';
-import EnvironmentTypePage from './pages/market/test/branching questions/EnvironmentType.tsx';
-import ImpulsiveTypePage from './pages/market/test/branching questions/ImpulsiveType.tsx';
-import MotivationalTypePage from './pages/market/test/branching questions/MotivationalType.tsx';
-import ProfilePAge from './pages/profile/Profile.tsx';
-import AiAnalysisPage from './pages/profile/anlysis/AiAnalysis.tsx';
-import ExpertAnalysisPage from './pages/profile/anlysis/ExpertAnalysis.tsx';
-import ExpertProfilePage from './pages/profile/anlysis/ExpertProfile.tsx';
-import ReportPage from './pages/report/Report.tsx';
-import RewardPage from './pages/reward/reward.tsx';
-import AttentionTypePage from './pages/market/test/branching questions/AttentionType.tsx';
+import "./index.css";
+import App from "./App.tsx";
+import LoginPage from "./pages/auth/Login.tsx";
+import SignupPage from "./pages/auth/Signup.tsx";
+import SplashPage from "./pages/auth/Splash.tsx";
+import GrowthPage from "./pages/growth/Growth.tsx";
+import MarketPage from "./pages/market/Market.tsx";
+import CartpagePage from "./pages/market/order/Cartpage.tsx";
+import CheckoutPage from "./pages/market/order/Checkout.tsx";
+import CheckoutSuccessPage from "./pages/market/order/CheckoutSuccess.tsx";
+import BranchingTestPage from "./pages/market/test/BranchingTest.tsx";
+import ComplexTypePage from "./pages/market/test/branching questions/ComplexType.tsx";
+import EmotionalTypePage from "./pages/market/test/branching questions/EmotionalType.tsx";
+import EnvironmentTypePage from "./pages/market/test/branching questions/EnvironmentType.tsx";
+import ImpulsiveTypePage from "./pages/market/test/branching questions/ImpulsiveType.tsx";
+import MotivationalTypePage from "./pages/market/test/branching questions/MotivationalType.tsx";
+import ProfilePAge from "./pages/profile/Profile.tsx";
+import AiAnalysisPage from "./pages/profile/anlysis/AiAnalysis.tsx";
+import ExpertAnalysisPage from "./pages/profile/anlysis/ExpertAnalysis.tsx";
+import ExpertProfilePage from "./pages/profile/anlysis/ExpertProfile.tsx";
+import ReportPage from "./pages/report/Report.tsx";
+import RewardPage from "./pages/reward/reward.tsx";
+import AttentionTypePage from "./pages/market/test/branching questions/AttentionType.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
