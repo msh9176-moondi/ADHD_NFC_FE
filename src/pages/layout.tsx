02a99b681974d12136251fbb233636fa70a878fe
imjane72-lab <imjane72@gmail.com>
2026-01-19 09:48:11 +0900

    fear: 광고 내용 보이게 설정

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -4,27 +4,25 @@ import { Outlet } from "react-router-dom";
 
 function RootLayout() {
   return (
-    <div className="flex">
-      {/* 왼쪽 광고 */}
-      <aside className="w-64 shrink-0">
-        <div className="left-44 top-1/2 -translate-y-1/2 pl-4">
+    <div className="flex justify-center">
+      {/* 왼쪽 광고 - 콘텐츠(412px) + 광고(256px) + 여백 = md(768px) 이상에서 표시 */}
+      <aside className="hidden md:block w-64 shrink-0">
+        <div className="pl-4">
           <div className="h-96 w-64 rounded-lg flex items-center justify-center border border-gray-200 bg-white">
             <Advertisement />
           </div>
         </div>
       </aside>
+
+      {/* 메인 영역 */}
       <div className="page">
-        {/* 메인 컨테이너 - max-width 412px */}
         <div className="container flex flex-col">
-          {/* 헤더 */}
           <AppHeader />
 
-          {/* 메인 영역 */}
           <main className="flex-1 w-full flex flex-col">
             <Outlet />
           </main>
 
-          {/* 푸터 */}
           <AppFooter />
         </div>
       </div>
