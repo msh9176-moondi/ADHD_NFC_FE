cf31b0c67703e5eb5dc25c2ef089fa5b91a23a90
imjane72-lab <imjane72@gmail.com>
2026-01-19 10:21:51 +0900

    광고영역 위치 변경

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -4,26 +4,29 @@ import { Outlet } from "react-router-dom";
 
 function RootLayout() {
   return (
-    <div className="flex justify-center">
-      {/* 왼쪽 광고 - 콘텐츠(412px) + 광고(256px) + 여백 = md(768px) 이상에서 표시 */}
-      <aside className="hidden md:block w-64 shrink-0">
-        <div className="pl-4">
-          <div className="h-96 w-64 rounded-lg flex items-center justify-center border border-gray-200 bg-white">
-            <Advertisement />
+    <div className="min-h-screen flex justify-center">
+      {/* 광고 + 콘텐츠 묶음 */}
+      <div className="flex gap-16">
+        {/* 왼쪽 광고 - 콘텐츠와 함께 중앙 정렬, 세로 중앙 고정 */}
+        <aside className="hidden md:block w-64 shrink-0 relative">
+          <div className="sticky top-1/2 -translate-y-1/2">
+            <div className="h-96 w-64 rounded-lg flex items-center justify-center border border-gray-200 bg-white">
+              <Advertisement />
+            </div>
           </div>
-        </div>
-      </aside>
+        </aside>
 
-      {/* 메인 영역 */}
-      <div className="page">
-        <div className="container flex flex-col">
-          <AppHeader />
+        {/* 메인 영역 */}
+        <div className="min-h-screen flex flex-col items-center">
+          <div className="w-[412px] max-w-full flex flex-col flex-1">
+            <AppHeader />
 
-          <main className="flex-1 w-full flex flex-col">
-            <Outlet />
-          </main>
+            <main className="flex-1 w-full flex flex-col">
+              <Outlet />
+            </main>
 
-          <AppFooter />
+            <AppFooter />
+          </div>
         </div>
       </div>
     </div>
